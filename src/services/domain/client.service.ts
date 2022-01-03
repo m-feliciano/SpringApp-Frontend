b74c95c698528ebe5f7de25bb29ba5e5adfdbab1
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-01-03 04:50:32 -0300

    interceptor including to token in requests

--- src/services/domain/client.service.ts
+++ src/services/domain/client.service.ts
@@ -12,11 +12,7 @@ export class ClientService {
     }
 
     findByEmail(email: string): Observable<ClientDTO> {
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({ 'Authorization': 'Bearer ' + token })
-        return this.http.get<ClientDTO>
-            (`${API_CONFIG.baseUrl}/clients/email?value=${email}`,
-                { 'headers': authHeader });
+        return this.http.get<ClientDTO>(`${API_CONFIG.baseUrl}/clients/email?value=${email}`)
     }
 
     getImageFromBucket(id: string): Observable<any> {
