3aad78c4f4765c0af3add2fb5e5e776513040c8a
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-01-11 21:58:02 -0300

    getting address from db

--- src/pages/address/address.ts
+++ src/pages/address/address.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AddressDTO } from '../../models/address.dto';
+import { ClientDTO } from '../../models/client.dto';
+import { ClientService } from '../../services/domain/client.service';
+import { StorageService } from '../../services/storage.service';
 
 @IonicPage()
 @Component({
@@ -14,25 +17,42 @@ export class AddressPage {
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams) {
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clientService: ClientService) {
   }
 
   ionViewDidLoad() {
-    this.items = [{
-      id: "1",
-      street: "Av Nove de Julho",
-      number: "300",
-      complement: "10 Floor",
-      district: "Centro",
-      cep: "09876-543",
-      city: {
-        id: "1",
-        name: "São Paulo",
-        state: {
-          id: "1",
-          name: "São Paulo"
-        }
-      }
-    }];
+    let localUser = this.storage.getLocalUser()
+    if (localUser && localUser.email) {
+      this.clientService.findByEmail(localUser.email)
+        .subscribe((response: ClientDTO) => {
+          this.items = response["address"];          //search image
+        }, (error: { status: number; }) => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        })
+    } else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 }
+
+
+// this.items = [{
+//   id: "1",
+//   street: "Av Nove de Julho",
+//   number: "300",
+//   complement: "10 Floor",
+//   district: "Centro",
+//   cep: "09876-543",
+//   city: {
+//     id: "1",
+//     name: "São Paulo",
+//     state: {
+//       id: "1",
+//       name: "São Paulo"
+//     }
+//   }
+// }];
--- src/services/domain/client.service.ts
+++ src/services/domain/client.service.ts
@@ -1,9 +1,9 @@
-import {HttpClient} from "@angular/common/http";
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/Rx";
-import {API_CONFIG} from "../../config/api.config";
-import {ClientDTO} from "../../models/client.dto";
-import {StorageService} from "../storage.service";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../../config/api.config";
+import { ClientDTO } from "../../models/client.dto";
+import { StorageService } from "../storage.service";
 
 @Injectable()
 export class ClientService {
@@ -11,13 +11,13 @@ export class ClientService {
     constructor(private http: HttpClient, private storage: StorageService) {
     }
 
-    findByEmail(email: string): Observable<ClientDTO> {
-        return this.http.get<ClientDTO>(`${API_CONFIG.baseUrl}/clients/email?value=${email}`)
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clients/email?value=${email}`);
     }
 
     getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType: "blob"})
+        return this.http.get(url, { responseType: "blob" })
     }
 
     insert(obj: ClientDTO) {
